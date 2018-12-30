fc9f6d2791e02f5be68500bb13bae1d0b429957a
Alexandra Drobut <Alexandra.Drobut@riverbed.com>
2018-12-30 23:14:24 +0200

    Implementing processing thread

--- facerec.cpp
+++ facerec.cpp
@@ -1,6 +1,59 @@
 #include "facerec.h"
 
-FaceRec::FaceRec()
+FaceRec::~FaceRec()
 {
 
+}
+
+
+bool FaceRec::is_running()
+{
+    return this->is_runnable;
+}
+
+void FaceRec::setFrame(cv::Mat new_frame)
+{
+    this->frame = new_frame;
+}
+
+void FaceRec::openCameraVideo()
+{
+        this->is_runnable = true;
+        start(LowPriority);
+}
+
+void FaceRec::run()
+{
+    int pred = 0;
+
+    while(this->is_runnable) {
+    if(frame.channels() == 3)
+        cv::cvtColor(frame,frame,CV_BGR2RGB);
+
+    pred = model->predict(frame);
+    qDebug() << pred;
+    if(pred)
+            cv::putText(frame,
+                QString::number(pred).toStdString(),
+                 cv::Point(40,40), // Coordinates
+                 cv::FONT_HERSHEY_COMPLEX_SMALL, // Font
+                 1.0, // Scale. 2.0 = 2x bigger
+                 cv::Scalar(150,0,30), // BGR Color
+                 5); // Anti-alias (Optional)
+
+    image = QImage((const unsigned char*)(frame.data),
+                   frame.cols,frame.rows,QImage::Format_Indexed8);
+     emit image_processed(image);
+     this->msleep(60);
+    }
+}
+
+void FaceRec::set_model(cv::Ptr<cv::face::FaceRecognizer> model)
+{
+    this->model = model;
+}
+
+void FaceRec::stop()
+{
+    this->is_runnable = false;
 }
--- facerec.h
+++ facerec.h
@@ -1,16 +1,47 @@
 #ifndef FACEREC_H
 #define FACEREC_H
+#include <QThread>
+#include <QMutex>
+#include <QImage>
+#include <QObject>
+#include <QWaitCondition>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/face.hpp>
+#include "opencv2/face/facerec.hpp"
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/video.hpp>
+#include <qdebug.h>
 
-
-class FaceRec : public QDeclarativeItem
+class FaceRec : public QThread
 {
-    Q_OBJECT
+    Q_OBJECT//thread aware holds an instance to the thread it was creatged into
 public:
-    FaceRec();
-
+    FaceRec(QObject *parent = 0) : QThread(parent), is_runnable(true) {}
+    void openCameraVideo();
+    void set_model(cv::Ptr<cv::face::FaceRecognizer>);
+    void setFrame(cv::Mat);
+    virtual ~FaceRec();
+    void run() override;
+    void stop();
+    void restart();
+    bool is_running();
 signals:
-
+    void image_processed(QImage);
 public slots:
+private:
+    bool is_runnable;
+    cv::Ptr<cv::face::FaceRecognizer> model;
+    cv::Mat frame;
+    cv::Mat g_frame;
+    QImage image;
+    bool stop_now;
+    QMutex mutex;
+    QWaitCondition condition;
+    cv::VideoCapture videosource;
 };
 
 #endif // FACEREC_H
