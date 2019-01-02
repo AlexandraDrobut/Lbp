df481101c48da6dcfa24b55bbfa450fa0c55e721
Alexandra Drobut <Alexandra.Drobut@riverbed.com>
2019-01-02 11:13:19 +0200

    This looks awful, howev, set background color

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.centralWidget()->setStyleSheet("background-color: gray;");
+    w.show();
     w.show();
 
     return a.exec();
